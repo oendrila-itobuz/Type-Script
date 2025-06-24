ade9b94e4eaad9ae47e483306a2209a460a1b9fa
oendrila-itobuz <oendrila@itobuz.com>
2025-06-24 13:21:59 -0700

    few more code snippets done

--- src/direction.ts
+++ src/direction.ts
@@ -11,3 +11,99 @@ export enum Direction {
 export const move=(direction: Direction): void => {
   console.log(`Moving ${direction}`);
 }
+//type any (not recommended)
+export const moveAny = (direction: any): void => {
+  console.log(`Moving ${direction}`);
+}
+//optional parameter might or might not be passed
+export const moveOptional = (direction: Direction, speed?: number): void => {
+  if (speed) {
+    console.log(`Moving ${direction} at speed ${speed}`);
+  } else {
+    console.log(`Moving ${direction}`);
+  }
+} 
+//generic function the function can accept any type of argument
+//and return the same type of argument
+//this is useful when you want to create a function that can work with any type of data
+//but the type will be constant throughout the function execution 
+export const identity = <T>(arg: T): T => {
+  return  arg;
+}
+
+//ts in mixins 
+
+type Constructor<T = {}> = new (...args: any[]) => T;
+class Naming {
+  constructor(public name: string) {}
+}
+
+function mixin<T extends Constructor<Naming>>(Base: T) {
+  return class extends Base {
+    getName() {
+      return `Name is: ${this.name}`;
+    }
+  };
+}
+const MixedNaming = mixin(Naming);
+const output = new MixedNaming("John Doe");
+console.log(output.getName()); 
+
+let age = 30;
+let newAge: typeof age; // newAge is of type number(captures the type of age)
+
+//const captures both type and value
+const greeting = "Hello World";
+let message: typeof greeting;
+message = "Hello World";     //  OK
+// message = "Hi there!";       Error: Only "Hello World" is allowed
+
+const colors = {
+  red: 'reddish',
+  blue: 'bluish',
+};
+
+type ColorsObject = typeof colors;
+
+// type ColorsObject = {
+//   red: string;
+//   blue: string;
+// };
+
+let colorSet: ColorsObject = {
+  red: 'bright red',
+  blue: 'deep blue'
+};
+
+// Keys as union type
+type ColorKeys = keyof typeof colors;
+// type ColorKeys = 'red' | 'blue';
+
+let colorName: ColorKeys;
+
+colorName = 'red';     
+// colorName = 'green'; error
+
+//interface
+interface Person {
+  name: string;
+  greet(): void;
+}
+
+const p: Person = {
+  name: 'Bob',
+  greet() {
+    console.log(`Hello, ${this.name}`);
+  }
+};
+
+p.greet(); 
+
+type UserID = number | string;
+let newId: UserID = 'abc123';
+
+//conditional types
+type IsString<T> = T extends string ? 'Yes' : 'No';
+type Result1 = IsString<string>; // 'Yes'
+type Result2 = IsString<number>; // 'No'
+
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { greet,Direction,move } from "./direction";
+import { greet,Direction,move,moveAny ,identity} from "./direction";
 
 greet("Hello World");
 move(Direction.Up);
+// move ("Down"); causes an error because "Down" is not a valid Direction enum value
+moveAny("Left");
+const a = identity<string>("This is a string");
+// identity<number>("This is a string"); will cause an error because the type is not consistent
+console.log(a); 
+// a=6 causes an error because a is a string and cannot be assigned a number
